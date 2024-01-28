1cb545a56f8496ebe53b623102140464cb5e9251
Mélaine <113093304+melaine69@users.noreply.github.com>
2024-01-28 11:54:47 +0100

    Ajout use-client page next

--- app/admin/_components/UserDropdown.tsx
+++ app/admin/_components/UserDropdown.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { LogOut, User } from "lucide-react";
