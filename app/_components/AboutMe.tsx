1b5f5aa314870d9cf0a07131f81a99d6702bad4e
Mélaine <113093304+melaine69@users.noreply.github.com>
2024-01-28 17:37:28 +0100

    Correction synthaxe

--- app/_components/AboutMe.tsx
+++ app/_components/AboutMe.tsx
@@ -14,8 +14,8 @@ export default function Aboutme() {
             <div className="mt-8 mx-auto grid md:grid-cols-2 gap-10 mb-10 p-10 rounded-3xl border border-slate-100 bg-white/50 backdrop-blur-lg drop-shadow-lg relative z-20">
                 <div>
                     <h2 className="mb-6 text-2xl font-bold">A propos</h2>
-                    <p className="text-lg text-justify font-medium">Je suis développeuse full-stack. J\'aime l\'aspect créatif du métier, le fait de construire un projet brique par brique et collaborer avec une équipe pluridisciplinaire.
-                        Je suis lyonnaise et j\'aime travailler en autonomie mais aussi en équipe.
+                    <p className="text-lg text-justify font-medium">Je suis développeuse full-stack. J&#39;aime l&#39;aspect créatif du métier, le fait de construire un projet brique par brique et collaborer avec une équipe pluridisciplinaire.
+                        Je suis lyonnaise et j&#39;aime travailler en autonomie mais aussi en équipe.
                     </p>
                     <div className='py-10'>
                         <Button
